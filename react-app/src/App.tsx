720e9bcad3c8f53e25d2ffaf5f42350ab71e025f
Dylan Lakhdari <emmelf.exe@gmail.com>
2026-06-11 09:52:52 +0200

    Add api vite project

---
+++ react-app/src/App.tsx
@@ -0,0 +1,23 @@
+// @ts-ignore
+import {Header} from "./components/Header.jsx";
+// @ts-ignore
+import {Footer} from "./components/Footer.jsx";
+
+function App() {
+
+    const items = ["Lorem", "Ipsum", "Dolores"];
+
+    const logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Vite_Logo_2026.svg/3840px-Vite_Logo_2026.svg.png"
+
+  return (
+        <div className="flex min-h-svh flex-col">
+            <Header listItems={items} logo={logo} />
+            <main className="flex flex-1 flex-col items-center justify-center bg-gray-50 p-6">
+              <h1 className="text-4xl font-bold mb-4">Bienvenue sur notre site</h1>
+            </main>
+            <Footer />
+        </div>
+  )
+}
+
+export default App
